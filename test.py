f4e209cca86781ecdc19faa42e78c30d48ba0524
Cierra Johnson-Carter <cierrajohnsoncarter@outlook.com>
2020-04-19 19:01:45 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+#Below are some print statements to test the functionality of this program
+
+#*Uncomment the push() statement below, there shouldn't be room for any more orders:
+#food_delivery.push("order #751228")
+
+# Uncomment the pop() statement below, this should remove an order from the stack and make room for one more order:
+#food_delivery.pop()
